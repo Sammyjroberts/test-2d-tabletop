2701adab8f45591d346d26979e85c6c63f6a3e76
kekeoki <omg.yeah.wai@hotmail.com>
2016-08-04 22:51:26 -0700

    initial

---
+++ Assets/boardManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class boardManager : MonoBehaviour
+{
+    public GameObject one;
+    public GameObject two;
+    public GameObject three;
+    private int rows = 10;
+    private int columns = 10;
+    private List<Vector3> gridPositions = new List<Vector3>();
+    // Use this for initialization
+    void Start()
+    {
+        for (int x = 1; x < columns - 1; x++)
+        {
+            //Within each column, loop through y axis (rows).
+            for (int y = 1; y < rows - 1; y++)
+            {
+                //At each index add a new Vector3 to our list with the x and y coordinates of that position.
+                gridPositions.Add(new Vector3(x, y, 0f));
+            }
+        }
+        for (int x = -1; x < columns + 1; x++)
+        {
+            //Loop along y axis, starting from -1 to place floor or outerwall tiles.
+            for (int y = -1; y < rows + 1; y++)
+            {
+                GameObject toInstantiate = one;
+                Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity);
+            }
+        }
+
+    }
+    void Update()
+    {
+    }
+}
---
+++ Assets/playerMover.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("casting");
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+
+            if (hit.collider != null)
+            {
+                transform.position = (hit.collider.transform.position);
+            }
+        }
+    }
+}
